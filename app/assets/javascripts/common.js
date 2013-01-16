855d6f18fd5dba1d53426ef14887b9a1d526115c
Kevin Xiwei Zheng <blankplacement@gmail.com>
2013-01-16 15:34:20 -0500

    Remove terms of service clickthrough

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -22,29 +22,19 @@ $(function() {
       $("#more-links").hide();
     });
   }
-  
+
   // Account notices
   $("#hide-sign-up-notice").click(function(e) {
     $("#sign-up-notice").hide();
     Danbooru.Cookie.put("hide_sign_up_notice", "1", 7);
     e.preventDefault();
   });
-  
+
   $("#hide-upgrade-account-notice").click(function(e) {
     $("#upgrade-account-notice").hide();
     Danbooru.Cookie.put('hide_upgrade_account_notice', '1', 7);
     e.preventDefault();
   });
-
-  // TOS link
-  if (!location.href.match(/terms_of_service/) && Danbooru.Cookie.get("tos") !== "1") {
-    var domain = location.href.match(/^(http:\/\/[^\/]+)/)[0];
-    location.href = domain + "/static/terms_of_service?url=" + location.href;
-  }
-  
-  $("#tos-agree-link").click(function() {
-    Danbooru.Cookie.put("tos", "1");
-  })
 });
 
 var Danbooru = {};
