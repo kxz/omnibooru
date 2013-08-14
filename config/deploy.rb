2ec89d7048972fa37d7768b546e330e8a7b4729d
Kevin Xiwei Zheng <blankplacement@gmail.com>
2013-08-13 21:48:00 -0400

    Deploy as the Danbooru user
    Trying to get Capistrano to work with different deployment and runtime
    users was ultimately just too much of a hassle.

--- config/deploy.rb
+++ config/deploy.rb
@@ -14,13 +14,13 @@ set :deploy_via, :remote_cache
 
 # Server
 set :application, "omnibooru"
+set :user, "danbooru"
 set :deploy_to, "/srv/danbooru2"
 set :shared_children, %w(public/cache public/data public/images/advertisements
                          log tmp/pids tmp/sockets)
 set :use_sudo, false
-default_run_options[:pty] = true
 set :default_environment, {
-  "PATH" => '/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH'
+  "PATH" => '/usr/local/rbenv/shims:$PATH'
 }
 
 # Multistage
@@ -28,23 +28,22 @@ set :stages, %w(production)
 set :default_stage, "production"
 
 # Bundler
-set :bundle_cmd, "/usr/local/rbenv/shims/bundle"
+set :bundle_cmd, "bundle"
 set :bundle_flags, "--deployment --binstubs"
 
 # Unicorn
 set :unicorn_env, defer {stage}
-set :unicorn_user, "danbooru"
 
 # Asset deployment
-set :rake, "#{sudo :as => unicorn_user} -E #{bundle_cmd} exec rake"
+set :rake, "#{bundle_cmd} exec rake"
 set :asset_env, "RAILS_RELATIVE_URL_ROOT=/booru RAILS_GROUPS=assets"
 
 # delayed_job
-set :delayed_job_command, "#{sudo :as => unicorn_user} #{bundle_cmd} exec ruby script/delayed_job"
+set :delayed_job_command, "#{bundle_cmd} exec script/delayed_job"
 set :delayed_job_args, "--queues=default,`hostname`"
 
 # Whenever
-set :whenever_command, "#{sudo :as => unicorn_user} #{bundle_cmd} exec whenever"
+set :whenever_command, "#{bundle_cmd} exec whenever"
 set :whenever_environment, defer {stage}
 
 
@@ -69,15 +68,6 @@ after "deploy:restart", "delayed_job:restart"
 
 # Deployment
 namespace :deploy do
-  namespace :assets do
-    desc "Fix permissions so that asset mtime updates work."
-    task :fix_permissions, :roles => :app do
-      run "#{sudo} chmod -R a+w #{shared_path}/#{shared_assets_prefix}"
-    end
-
-    after "deploy:assets:precompile", "deploy:assets:fix_permissions"
-  end
-
   desc "Create the shared config directory."
   task :setup_config, :roles => :app do
     run "mkdir -p #{shared_path}/config"
@@ -92,14 +82,6 @@ namespace :deploy do
   after "deploy:setup", "deploy:setup_config"
   after "deploy:setup", "deploy:setup_data_dirs"
 
-  desc "Change permissions of files in the release directory that need to be writable by the Unicorn user."
-  task :fix_permissions, :roles => :app do
-    %w(. db/structure.sql log tmp).map do |path|
-      run "#{sudo} chown #{unicorn_user} #{release_path}/#{path}"
-      run "#{sudo} chmod g+w #{release_path}/#{path}"
-    end
-  end
-
   desc "Create symlinks to config files in the shared directory in the release directory."
   task :symlink_config, :roles => :app do
     %w(danbooru_local_config.rb database.yml secret_token session_secret_key).map do |file|
@@ -107,7 +89,6 @@ namespace :deploy do
     end
   end
 
-  after "deploy:update_code", "deploy:fix_permissions"
   after "deploy:finalize_update", "deploy:symlink_config"
 end
 
