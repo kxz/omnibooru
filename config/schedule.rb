98ad5aee30320b2df83a96d4c5c7f8026508eca9
Kevin Xiwei Zheng <blankplacement@gmail.com>
2014-07-01 11:11:56 -0400

    Remove redundant PostgreSQL vacuum scheduled task

--- config/schedule.rb
+++ config/schedule.rb
@@ -7,7 +7,6 @@ end
 every 1.day, :at => "1:00 am" do
   script "donmai/backup_db"
   script "donmai/prune_backup_dbs"
-  command "psql --set statement_timeout=0 -c \"vacuum analyze;\" danbooru2"
 end
 
 every 1.week, :at => "1:30 am" do
