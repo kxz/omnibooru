6e8b02b51b92fa11a9aa9c262ef8afb956f89ebc
Kevin Xiwei Zheng <blankplacement@gmail.com>
2015-08-20 00:23:19 -0400

    Some more Capistrano fixes

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,13 +2,16 @@ set :stages, %w(production)
 set :default_stage, "production"
 set :application, "omnibooru"
 set :repo_url, "git://github.com/kxz/omnibooru.git"
+set :branch, "omnibooru"
 set :scm, :git
 set :deploy_to, "/srv/danbooru2"
 set :rbenv_ruby, "2.1.5"
+set :rbenv_type, :system
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
 set :default_env, {
   "RAILS_RELATIVE_URL_ROOT" => "/booru",
-  "RAILS_GROUPS" => "assets"
+  "RAILS_GROUPS" => "assets",
+  "XDG_RUNTIME_DIR" => "/run/user/112",
 }
 
 after "deploy:symlink:shared", "symlink:local_files"
